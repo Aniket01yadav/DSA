b3e5efa91ca5e40471303a5ba39718c72dbf185f
Aniket Kumar <134836170+Aniket01yadav@users.noreply.github.com>
2026-09-19 22:48:07 +0530

    Time: 27 ms (33.91%), Space: 19.7 MB (35.04%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,22 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        dict1 = {}
-        dict2 = {}
-
-        for i in ransomNote:
-            if i in dict1:
-                dict1[i] += 1
+        counts = defaultdict(int)
+        for letter in magazine:
+            counts[letter] += 1
+        
+        for letter in ransomNote:
+            if counts[letter] > 0:
+                counts[letter] -= 1
             else:
-                dict1[i] = 1
-
-        for i in magazine:
-            if i in dict2:
-                dict2[i] += 1
-            else:
-                dict2[i] = 1
-            
-        for key in dict1:
-            if key not in dict2 or dict2[key] < dict1[key]:
                 return False
 
         return True
